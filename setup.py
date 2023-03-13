039c864581e4aa8cd99d7ea3a0e9cfc5a72621e2
David Ung <david.unga@gmail.com>
2023-03-13 16:06:17 +0200

    add setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='infotheo',
+    description='Information Theory utilities',
+    version='1.0.0',
+    packages=find_packages(),
+    author='David Ungarish'
+)
